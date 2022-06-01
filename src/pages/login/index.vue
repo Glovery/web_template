265a7014fa3d91563a1079774a55b9ab345b80a1
Glovery <583009863@qq.com>
2022-06-01 17:16:11 +0800

    feat: 登录页面

---
+++ src/pages/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login-page"></div>
+</template>
+<script setup>
+import { defineComponent } from 'vue'
+
+defineComponent({
+    name: 'LoginPage',
+})
+</script>
+<style lang="scss" scoped>
+.login-page {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: #ededed;
+}
+</style>
